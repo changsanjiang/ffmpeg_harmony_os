72bd5f5d435857ef11551358bff448faf26fea1d
changsanjiang <changsanjiang@gmail.com>
2024-11-10 17:45:13 +0800

    Update client_print.cpp

--- ffmpeg/src/main/cpp/extension/client_print.cpp
+++ ffmpeg/src/main/cpp/extension/client_print.cpp
@@ -7,17 +7,29 @@
 #include "napi/native_api.h"
 #include "client_print.h"
 #include "utils.h"
+#include <stdio.h>
+#include <string.h>
 
 EXTERN_C_START
-static napi_env env_prt = nullptr;
-static napi_ref print_handler_ref = nullptr; // 打印回调函数: (msg) => void
+static napi_threadsafe_function print_handler_fn = nullptr; // 打印回调函数: (msg) => void
+
+// 主线程的回调函数
+void CompleteTask(napi_env env, napi_value js_cb, void* context, void* data) {
+    char* message = (char*)data;
+
+    // 调用 JavaScript 回调
+    napi_value undefined, result;
+    napi_get_undefined(env, &undefined);
+    napi_create_string_utf8(env, message, NAPI_AUTO_LENGTH, &result);
+    napi_call_function(env, undefined, js_cb, 1, &result, NULL);
+    delete [] message;
+}
 
 napi_value 
 native_set_client_print_handler(napi_env env, napi_callback_info info) {
-    env_prt = env;
-    
-    if ( print_handler_ref != nullptr ) {
-        napi_delete_reference(env_prt, print_handler_ref);
+    if (print_handler_fn) {
+        napi_release_threadsafe_function(print_handler_fn, napi_tsfn_release);
+        print_handler_fn = nullptr;
     }
     
     size_t argc = 1;
@@ -25,27 +37,39 @@ native_set_client_print_handler(napi_env env, napi_callback_info info) {
     napi_value args[argc];
     // 获取传入的参数
     napi_get_cb_info(env, info, &argc, args, nullptr, nullptr);
+    
+    napi_valuetype valuetype;
+    napi_typeof(env, args[print_handler_index], &valuetype);
+    if (valuetype != napi_function) {
+        napi_throw_type_error(env, nullptr, "Expected a function as the argument");
+        return nullptr;
+    }
+    
+    // 设置 async_resource_name
+    napi_value async_resource_name;
+    napi_create_string_utf8(env, "print_handler", NAPI_AUTO_LENGTH, &async_resource_name);
 
-    // 获取打印处理函数
-    napi_create_reference(env, args[print_handler_index], 1, &print_handler_ref);
+    // 创建线程安全函数
+    napi_status status = napi_create_threadsafe_function(env, args[print_handler_index], nullptr, async_resource_name, 0, 1, nullptr, nullptr, nullptr, CompleteTask, &print_handler_fn);
+
+    if (status != napi_ok) {
+        char msg[128];
+        sprintf(msg, "Failed to create threadsafe function(%d)", status);
+        napi_throw_error(env, nullptr, msg);
+    }
     return nullptr;
 }
 
 /// 客户端打印消息
 void client_print_message(const char *msg) {
-    if ( env_prt == nullptr ) return;
-    if ( print_handler_ref == nullptr ) return;
-    napi_value print_handler_value;
-    napi_get_reference_value(env_prt, print_handler_ref, &print_handler_value);
-
-    // 将 C 字符串转换为 napi_value
-    napi_value msg_value;
-    napi_create_string_utf8(env_prt, msg, NAPI_AUTO_LENGTH, &msg_value);
-
-    // 调用回调函数
-    napi_value global_value;
-    napi_get_global(env_prt, &global_value);
-    napi_call_function(env_prt, global_value, print_handler_value, 1, &msg_value, nullptr);
+    if ( print_handler_fn == nullptr ) return;
+    char *heap_message = new char [strlen(msg) + 1];
+    strcpy(heap_message, msg);
+    // 调用线程安全函数，传递数据给 JavaScript 主线程
+    napi_status status = napi_call_threadsafe_function(print_handler_fn, heap_message, napi_tsfn_nonblocking);
+    if (status != napi_ok) {
+        delete [] heap_message;
+    }
 }
 
 /// 客户端打印消息
