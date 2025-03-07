a831b814a82d99591c1f6d61218c74b435cc32e8
changsanjiang <changsanjiang@gmail.com>
2025-03-07 11:43:34 +0800

    add FFAudioMultiStreamPlayer

--- ffmpeg/src/main/cpp/napi_init.cpp
+++ ffmpeg/src/main/cpp/napi_init.cpp
@@ -15,6 +15,7 @@
     along with @sj/ffmpeg. If not, see <http://www.gnu.org/licenses/>.
  * */
 #include "general/FFAbortController.h"
+#include "general/FFAudioMultiStreamPlayer.h"
 #include "general/FFAudioPlayer.h"
 #include "general/FFPlayWhenReadyChangeReason.h"
 #include "general/FFmpeg.h"
@@ -27,6 +28,7 @@ static napi_value Init(napi_env env, napi_value exports)
     FFAV::FFAudioPlayer::Init(env, exports);
     FFAV::FFAbortController::Init(env, exports);
     FFAV::FFPlayWhenReadyChangeReason::Init(env, exports);
+    FFAV::FFAudioMultiStreamPlayer::Init(env, exports);
     return exports;
 }
 EXTERN_C_END
